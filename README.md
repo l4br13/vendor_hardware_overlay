093691ba8495fec8c71cfac3df1fc24ed65cd4f2
l4br13 <l4br13@outlook.com>
2025-02-21 19:41:26 +0700

    Fri Feb 21 07:41:26 PM WIB 2025

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Vendor hardware overlay POCO M4 Pro 4G (fleur)
+# Vendor hardware overlay personal use
 This is test / experimental, do not use this overlay on your device it might be unstable.
 
 See on [vendor_hardware_overlay](https://github.com/TrebleDroid/vendor_hardware_overlay) for list fixed supported device overlay
--- build.sh
+++ build.sh
@@ -9,9 +9,18 @@ name=treble-overlay-xiaomi-fleur
 
 path=$(dirname $0)
 
-echo "Building $name"
+for i in $path/devices/*;
+do
+	if [ -f $i/AndroidManifest.xml ]; then
+		echo "Building $(basename $i).apk"
+		aapt package -f -F "$(dirname $0)/$(basename $i)-unsigned.apk" -M "$i/AndroidManifest.xml" -S "$i/res" -I android.jar
+		LD_LIBRARY_PATH=./signapk/ java -jar signapk/signapk.jar keys/platform.x509.pem keys/platform.pk8 "$(basename $i)-unsigned.apk" "$(basename $i).apk"
+		rm -f "$(basename $i)-unsigned.apk"
+	fi
+done
 
-aapt package -f -F "${name}-unsigned.apk" -M "$path/AndroidManifest.xml" -S "$path/res" -I android.jar
+#echo "Building $name"
 
-LD_LIBRARY_PATH=./signapk/ java -jar signapk/signapk.jar keys/platform.x509.pem keys/platform.pk8 "${name}-unsigned.apk" "${name}.apk"
-rm -f "${name}-unsigned.apk"
+#aapt package -f -F "${name}-unsigned.apk" -M "$path/AndroidManifest.xml" -S "$path/res" -I android.jar
+
+#rm -f "${name}-unsigned.apk"
